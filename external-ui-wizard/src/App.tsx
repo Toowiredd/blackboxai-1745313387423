447267800d7d44eaaa834ac3694b6772bcba65c7
Toowiredd <lewistys420@gmail.com>
2025-04-22 11:02:01 +0000

    Add external-ui-wizard/src/App.tsx 1745319721465

---
+++ external-ui-wizard/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wizard from './components/Wizard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
+      <Wizard />
+    </div>
+  );
+}
+
+export default App;
